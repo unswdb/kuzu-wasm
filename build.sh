573afec0b67a0a19fcd3271a5654d6f6b43db9ca
Dylan Shang <1094525396@qq.com>
2024-04-12 18:18:28 +1000

    support debug features

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 emcmake cmake \
     -B build \
-    -DCMAKE_BUILD_TYPE=Release . &&
+    -DCMAKE_BUILD_TYPE=Debug . &&
 emmake make \
     -C build \
     EMCC_CFLAGS="-Wno-c++11-narrowing"\
